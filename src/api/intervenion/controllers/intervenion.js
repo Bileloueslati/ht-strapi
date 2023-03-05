b1ccee1c2881110d01e8a428101a1b1cc9298eed
Bilel <b.oueslati@ewm.swiss>
2023-03-05 12:08:36 +0100

    init projet

---
+++ src/api/intervenion/controllers/intervenion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * intervenion controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::intervenion.intervenion');
---
+++ src/api/intervenion/services/intervenion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * intervenion service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::intervenion.intervenion');
